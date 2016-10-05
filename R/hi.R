3fd60d00b68d12f8b0e1c25d5d6d34ecfda8ebda
Bios <bios@Bios.local>
2016-10-05 17:41:56 -0400

    "shout" to uppercase, "whisper" to lowercase.

--- R/hi.R
+++ R/hi.R
@@ -2,21 +2,27 @@
 #'
 #' some helpful text.
 #' @param who character(1) The name of the person for conversation
+#' @param how character(1) How to greet the conversant. "shout" or "whisper".
 #' @return character(1) conversation starter
 #' @examples
 #'    hi("Qian Liu")
+#'    hi("Qian Liu", "whisper")
 #' @export
 #'
-hi = function(who){
+hi = function(who, how=c("shout", "whisper")){
   stopifnot(
     is.character(who),
     length(who) == 1,
     !is.na(who)
   )
-  paste("hello", who, ", you have", nchar(who), "letters in your name")
+  how = match.arg(how)
+  fun = switch(how, shout=shout, whisper=whisper)
+  paste("hello", fun(who), ", you have", nchar(who), "letters in your name")
 }
 
 shout = function(who){
-  paste("hello", shout(who), "you have", nchar(who), "letters in your name")
-
+  toupper(who)
+}
+whisper = function(who){
+  tolower(who)
 }
