cf3f61324e3fad823d26ea3a555173dcc27a3bf6
Bios <bios@Bios.local>
2016-10-05 17:29:54 -0400

    check input to hi()

--- R/hi.R
+++ R/hi.R
@@ -1,17 +1,22 @@
 #' This function is a conversation starter.
-#' 
+#'
 #' some helpful text.
 #' @param who character(1) The name of the person for conversation
 #' @return character(1) conversation starter
 #' @examples
 #'    hi("Qian Liu")
 #' @export
-#' 
+#'
 hi = function(who){
+  stopifnot(
+    is.character(who),
+    length(who) == 1,
+    !is.na(who)
+  )
   paste("hello", who, ", you have", nchar(who), "letters in your name")
 }
 
 shout = function(who){
   paste("hello", shout(who), "you have", nchar(who), "letters in your name")
-  
+
 }
