e66cb8c8cf3452c66f2d8496bd3269def7d0af2e
Bios <bios@Bios-2.local>
2016-10-07 17:53:49 -0400

    implement 'input_tidy'
    -represent data as tbl_df

---
+++ R/tidy.R
@@ -0,0 +1,20 @@
+#' Represent data in a 'tidy' format
+#'
+#' 'Tidy' data takes a 'long' view of data, where each variable is a column.
+#' @details This approach popularized by Hadley Wickham; see the vignette in the package 'dplyr'
+#'
+#' @importFrom reshape2 melt
+#' @importFrom dplyr tbl_df
+#' @importFrom magrittr %>%
+#' @param pdata_file character(1) The path to the pheno data file.
+#' @param exprs_file character(1) The path to the expression file.
+#' @return exprs_file character(1) The path to the expression file.
+#' @export
+#'
+
+input_tidy = function(pdata_file, exprs_file) {
+  classic = input_classic(pdata_file, exprs_file)
+  melt(classic, id.vars=1:22, variable.name="probeset",
+       value.name = "exprs") %>%
+         tbl_df
+}
