9326d479689448fb06859f7e510efae855e0b2a3
Bios <bios@Bios-2.local>
2016-10-14 16:44:45 -0400

    more functions.
    -- check the prerequest for input files.
    -- check()
    -- white space (not tab) before @param.

--- R/classic.R
+++ R/classic.R
@@ -1,19 +1,20 @@
 #' classic R approaches to data analysis
 #' the functions here illustrate how an "old-school" R users might approach data management and analysis.
 #'
-#'  @details There are 2 files. They are from a microarray experiment. The first file,
+#' @details There are 2 files. They are from a microarray experiment. The first file,
 #' "All-phenoData.csv" describes the 128 samples. The second file, are the normalized expressions
 #'  values for 12k probesets across 128 samples. The files originally came from sheets in excel, and were
 #'  from Excel using export-to-csv.
-#'
-#'  @param pdata_file character(1) The path to the pheno data file.
-#'  @param exprs_file character(1) The path to the expression file.
-#'
-#'  @return A data.frame() containing samples as rows and phenotype data and expression data as columns.
-#'  @export
+#' @param pdata_file character(1) The path to the pheno data file.
+#' @param exprs_file character(1) The path to the expression file.
+#' @return A data.frame() containing samples as rows and phenotype data and expression data as columns.
+#' @importFrom utils read.csv
+#' @export
 #'
 
 input_classic = function(pdata_file, exprs_file){
+  stopifnot(is_scalar_character(pdata_file))
+  stopifnot(is_scalar_character(exprs_file))
   pdata = read.csv(pdata_file, row.names=1, check.names=FALSE)
   exprs = read.csv(exprs_file, row.names=1, check.names=FALSE)
   merge(pdata, t(exprs), by=0)   ## merge using the rownames
